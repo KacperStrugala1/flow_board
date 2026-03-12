a1fa5a6fb5b7c1ce8b902632e6e421c1bcf69dff
KacperStrugala1 <kacperstrugala5@gmail.com>
2026-03-12 08:26:32 +0100

    Added statuses

--- flow_board/flow_board/models.py
+++ flow_board/flow_board/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+class Status(models.TextChoices):
+    STARTED = "S", "STARTED"
+    PROGRESS = "P", "IN PROGRESS"
+    DONE = "D", "DONE"
+    
+
 class Task(models.Model):
+    
     name = models.CharField(max_length=50)
     description = models.TextField()
     category = models.CharField(max_length=30)
+    status = models.CharField(
+        max_length=1,
+        choices=Status.choices, 
+        default=Status.STARTED
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
