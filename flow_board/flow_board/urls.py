926235d70da9d1b936c7ab0ad7673ccc6c0abde1
kstrugal <kacper.strugala@nokia.com>
2026-03-19 19:51:38 +0100

    Added styles

--- flow_board/flow_board/urls.py
+++ flow_board/flow_board/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name="home_view"),
     #<int:value> - value the same as value provided to view
     path('task/<int:pk>', views.TaskView.as_view(), name="task_view"),
+    path('tasks/', views.TaskApiView.as_view(), name='tasks'),
 ]
--- flow_board/flow_board/views.py
+++ flow_board/flow_board/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import render, redirect
 from django.views import View
 from .models import Task
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializer import TaskSerializer
 
 class HomeView(View):
     template_name = "home.html"
@@ -26,3 +29,22 @@ class TaskView(View):
         task = get_object_or_404(Task, pk=pk)
         
         return render(request, self.template_name, {"task":task})
+
+    def post(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        task.status = request.POST.get("statuses")
+        task.save()
+        return redirect("task_view", pk=pk)
+
+class TaskApiView(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
